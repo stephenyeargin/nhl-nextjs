64cd831195b713ff43476554fb0d1ee2cd632fe7
Stephen Yeargin <stephen@yearg.in>
2025-01-18 00:12:45 -0600

    Analytics tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ import TopBarSchedule from '@/app/components/TopBarSchedule';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import './globals.css';
+import MatomoAnalytics from './components/MatomoAnalytics';
 
 config.autoAddCss = false;
 
@@ -40,6 +41,7 @@ export default function RootLayout({ children }) {
         <div className="mt-20 bg-slate-200 dark:bg-slate-800 p-5">
           <div className="text-xs text-center">All trademarks are property of their respective owners. | <Link href="https://github.com/stephenyeargin/nhl-nextjs" className="font-bold underline">Source Code</Link></div>
         </div>
+        <MatomoAnalytics url={process.env.MATOMO_URL} siteId={process.env.MATOMO_SITE_ID} />
       </body>
     </html>
   );
