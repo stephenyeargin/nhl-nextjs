2c93c2bd4044887104a28afa901a68c98f1d0a9f
Stephen Yeargin <stephen@yearg.in>
2025-03-27 09:21:30 -0500

    Add legend

--- src/app/standings/page.js
+++ src/app/standings/page.js
@@ -30,6 +30,24 @@ export default async function Home() {
       <StandingsTable standings={westernConference} />
       <h2 className="text-xl py-4">Eastern Conference</h2>
       <StandingsTable standings={easternConference} />
+      
+      <hr className="my-5" />
+      <div className="flex gap-1">
+        <div className="font-bold">Legend:</div>
+        <dl className="flex gap-1 align-middle text-xs">
+          <dt className="p-1 font-bold">(x)</dt>
+          <dd className="p-1">Clinched Playoff spot</dd>
+          <dt className="p-1 font-bold">(y)</dt>
+          <dd className="p-1">Clinched Division</dd>
+          <dt className="p-1 font-bold">(p)</dt>
+          <dd className="p-1">Presidents&apos; Trophy</dd>
+          <dt className="p-1 font-bold">(z)</dt>
+          <dd className="p-1">Clinched Conference</dd>
+          <dt className="p-1 font-bold">(e)</dt>
+          <dd className="p-1">Eliminated</dd>
+        </dl>
+      </div>
+
     </div>
   );
 }
