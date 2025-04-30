1ee58d2e1501aaf5541f6b684157bc6eb6ca4158
Stephen Yeargin <stephen@yearg.in>
2025-04-30 13:48:00 -0500

    Update tile to handle TBD more appropriately

--- src/app/components/PlayoffSeriesTile.js
+++ src/app/components/PlayoffSeriesTile.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import TeamLogo from './TeamLogo';
 import { PropTypes } from 'prop-types';
 
-const TBDLogo = () => (<Image src="https://assets.nhle.com/logos/nhl/svg/team-tbd-light.svg" width="1024" height="1024" alt="To Be Determined" className="w-8 h-8 mr-3" />);
+const TBDLogo = ({ className }) => (<Image src="https://assets.nhle.com/logos/nhl/svg/team-tbd-light.svg" width="1024" height="1024" alt="To Be Determined" className={className || 'w-8 h-8 mr-3'} />);
 
 const PlayoffSeriesTile = ({ series, year }) => {
   if (!series) {
@@ -22,27 +22,6 @@ const PlayoffSeriesTile = ({ series, year }) => {
     );
   }
 
-  if (!series.topSeedTeam) {
-    return (
-      <Link
-        href={`/playoffs/${year}/${series.seriesLetter?.toLowerCase()}`}
-        className="text-xs p-2 border rounded-lg shadow my-4 flex items-center bg-white dark:bg-gray-900 text-black dark:text-white"
-      >
-        <div className="flex-1">
-          <div className="flex items-center justify-center md:justify-start">
-            <TBDLogo />
-            <div className="hidden md:block grow font-bold">TBD</div>
-          </div>
-
-          <div className="flex items-center justify-center md:justify-start">
-            <TBDLogo />
-            <div className="hidden md:block grow font-bold">TBD</div>
-          </div>
-        </div>
-      </Link>
-    );
-  }
-
   const top = series.topSeedTeam;
   const bottom = series.bottomSeedTeam;
 
@@ -55,24 +34,44 @@ const PlayoffSeriesTile = ({ series, year }) => {
           className="text-xs p-2 border rounded-lg shadow my-4 bg-white dark:bg-gray-900 text-black dark:text-white block"
         >
           <div className="flex flex-wrap gap-5 justify-center">
-            <div className={`text-center ${(series.losingTeamId === top.id) ? 'opacity-50' : ''}`}>
-              <TeamLogo src={top.logo} alt={top.abbrev} className="w-20 h-20 mx-auto" noLink />
-              <div>
-                <span className="font-thin">({series.topSeedRankAbbrev})</span>
-                {' '}
-                <span className="font-bold">{top.commonName?.default.replace(top.placeNameWithPreposition?.default, 'TBD')}</span>
-              </div>
-              <div className="font-bold text-xl py-2">{series.topSeedWins}</div>
+            <div className={`text-center ${(top && series.losingTeamId === top.id) ? 'opacity-50' : ''}`}>
+              {top && top.id !== -1 ? (
+                <>
+                  <TeamLogo src={top.logo} alt={top.abbrev} className="w-20 h-20 mx-auto" noLink />
+                  <div>
+                    <span className="font-thin">({series.topSeedRankAbbrev})</span>
+                    {' '}
+                    <span className="font-bold">{top.commonName?.default.replace(top.placeNameWithPreposition?.default, 'TBD')}</span>
+                  </div>
+                  <div className="font-bold text-xl py-2">{series.topSeedWins}</div>
+                </>
+              ) : (
+                <>
+                  <TBDLogo className='w-20 h-20 mx-auto' />
+                  <div className="font-bold">TBD</div>
+                  <div className="font-bold text-xl py-2">0</div>
+                </>
+              )}
             </div>
 
-            <div className={`text-center ${series.losingTeamId === bottom.id ? 'opacity-50' : ''}`}>
-              <TeamLogo src={bottom.logo} alt={bottom.abbrev} className="w-20 h-20 mx-auto" noLink />
-              <div>
-                <span className="font-thin">({series.bottomSeedRankAbbrev})</span>
-                {' '}
-                <span className="font-bold">{bottom.commonName?.default.replace(bottom.placeNameWithPreposition?.default, 'TBD')}</span>
-              </div>
-              <div className="font-bold text-xl py-2">{series.bottomSeedWins}</div>
+            <div className={`text-center ${(bottom && series.losingTeamId === bottom.id) ? 'opacity-50' : ''}`}>
+              {bottom && bottom.id !== -1 ? (
+                <>
+                  <TeamLogo src={bottom.logo} alt={bottom.abbrev} className="w-20 h-20 mx-auto" noLink />
+                  <div>
+                    <span className="font-thin">({series.bottomSeedRankAbbrev})</span>
+                    {' '}
+                    <span className="font-bold">{bottom.commonName?.default.replace(bottom.placeNameWithPreposition?.default, 'TBD')}</span>
+                  </div>
+                  <div className="font-bold text-xl py-2">{series.bottomSeedWins}</div>
+                </>
+              ) : (
+                <>
+                  <TBDLogo className='w-20 h-20 mx-auto' />
+                  <div className="font-bold">TBD</div>
+                  <div className="font-bold text-xl py-2">0</div>
+                </>
+              )}
             </div>
           </div>
         </Link>
@@ -87,23 +86,47 @@ const PlayoffSeriesTile = ({ series, year }) => {
         className="text-xs p-2 border rounded-lg shadow my-4 flex items-center bg-white dark:bg-gray-900 text-black dark:text-white"
       >
         <div className="flex-1">
-          <div className={`flex items-center justify-center xl:justify-start ${(series.losingTeamId === top.id) ? 'opacity-50' : ''}`}>
-            <TeamLogo src={top.logo} alt={top.abbrev} className="w-8 h-8 mr-3" noLink />
-            <div className="hidden xl:block mx-1 font-thin">({series.topSeedRankAbbrev})</div>
-            <div className="hidden xl:block grow font-bold">{top.commonName?.default.replace(top.placeNameWithPreposition?.default, 'TBD')}</div>
-            <span className="font-semibold">{series.topSeedWins}</span>
+          <div className={`flex items-center justify-center xl:justify-start ${(top && series.losingTeamId === top?.id) ? 'opacity-50' : ''}`}>
+            {top && top.id !== -1 ? (
+              <>
+                <TeamLogo src={top.logo} alt={top.abbrev} className="w-8 h-8 mr-3" noLink />
+                <div className="hidden xl:block mx-1 font-thin">({series.topSeedRankAbbrev})</div>
+                <div className="hidden xl:block grow font-bold">{top.commonName?.default.replace(top.placeNameWithPreposition?.default, 'TBD')}</div>
+                <div className="font-semibold">{series.topSeedWins}</div>
+              </>
+            ) : (
+              <>
+                <TBDLogo />
+                <div className="hidden xl:block grow font-bold">TBD</div>
+                <div className="font-semibold">0</div>
+              </>
+            )}
           </div>
 
-          <div className={`flex items-center justify-center xl:justify-start ${series.losingTeamId === bottom.id ? 'opacity-50' : ''}`}>
-            <TeamLogo src={bottom.logo} alt={bottom.abbrev} className="w-8 h-8 mr-3" noLink />
-            <div className="hidden xl:block mx-1 font-thin">({series.bottomSeedRankAbbrev})</div>
-            <div className="hidden xl:block grow font-bold">{bottom.commonName?.default.replace(bottom.placeNameWithPreposition?.default, 'TBD')}</div>
-            <div className="font-semibold">{series.bottomSeedWins}</div>
+          <div className={`flex items-center justify-center xl:justify-start ${bottom && series.losingTeamId === bottom?.id ? 'opacity-50' : ''}`}>
+            {bottom && bottom.id !== -1 ? (
+              <>
+                <TeamLogo src={bottom.logo} alt={bottom.abbrev} className="w-8 h-8 mr-3" noLink />
+                <div className="hidden xl:block mx-1 font-thin">({series.bottomSeedRankAbbrev})</div>
+                <div className="hidden xl:block grow font-bold">{bottom.commonName?.default.replace(bottom.placeNameWithPreposition?.default, 'TBD')}</div>
+                <div className="font-semibold">{series.bottomSeedWins}</div>
+              </>
+            ) : (
+              <>
+                <TBDLogo />
+                <div className="hidden xl:block grow font-bold">TBD</div>
+                <div className="font-semibold">0</div>
+              </>
+            )}
           </div>
         </div>
       </Link>
     </>
   );
+};
+
+TBDLogo.propTypes = {
+  className: PropTypes.string,
 };
 
 PlayoffSeriesTile.propTypes = {
